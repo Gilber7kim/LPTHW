7188069aafd4354d13c2165e92274bcfe6f9bc19
Gilbert Kim <gilbertkim@seoulchristian.org>
2016-01-14 15:42:09 +0900

    Fix errors in LPTHW ex35

--- ex35.py
+++ ex35.py
@@ -1,5 +1,6 @@
 from sys import exit
 
+
 def gold_room():
     print "This room is full of gold.  How much do you take?"
 
@@ -29,7 +30,7 @@ def bear_room():
         if choice == "take honey":
             dead("The bear looks at you then slaps your face off.")
         elif choice == "taunt bear" and not bear_moved:
-            print "The bear has moved from the door. You can go through it now."
+            print"The bear has moved from the door. You can go through it now."
             bear_moved = True
         elif choice == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
@@ -57,6 +58,7 @@ def cthulhu_room():
 def dead(why):
     print why, "Good job!"
     exit(0)
+
 
 def start():
     print "You are in a dark room."

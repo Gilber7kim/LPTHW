19387188088e4776be0404e22be7500959c7a41c
Gilbert Kim <gilbertkim@seoulchristian.org>
2016-01-21 23:56:52 +0900

    Finish ex36

---
+++ ex36.py
@@ -0,0 +1,125 @@
+from random import randint
+import time
+import os,sys
+import sys,string
+import string
+
+
+def selfIntroduce():
+    print "Hello, this is Gilbert."
+    name = raw_input("What is your name? ")
+    print "So your name is "+name+". Welcome "+name+"!"
+
+    if "David" in name:
+        stupid_david()
+    else:
+        permission()
+
+def permission():
+    print "Do you want to play a game with me? [y/n]"
+    choice = raw_input("> ")
+
+    if choice == "y":
+        dice()
+    elif choice == "n":
+        print("bye!")
+        exit(0)
+    else:
+        print("Why can't you type y or n? I don't want to play with dumb. Bye!")
+        exit(0)
+
+
+def stupid_david():
+    print "Oh, actually all the Davids cannot play this game. Blame David lee :)"
+    exit(0)
+
+
+def dice():
+    import random
+    print("Win me with the dice.")
+    print("You should have a higher number than three.")
+    raw_input("Enter to continue")
+    number = random.randint (1,6)
+    if number==1:
+        print ("[----------------]")
+        print ("[----------------]")
+        print ("[-------()-------]")
+        print ("[----------------]")
+        print ("[----------------]")
+        print("Jump out.")
+        exit(0)
+    if number==2:
+        print ("[----------------]")
+        print ("[----------------]")
+        print ("[--()--------()--]")
+        print ("[----------------]")
+        print ("[----------------]")
+        print("Loser.")
+        exit(0)
+    if number==3:
+        print ("[-()-------------]")
+        print ("[----------------]")
+        print ("[-------()-------]")
+        print ("[----------------]")
+        print ("[-------------()-]")
+        print("That was close, dude.")
+        exit(0)
+    if number==4:
+        print ("[-()----------()-]")
+        print ("[----------------]")
+        print ("[----------------]")
+        print ("[----------------]")
+        print ("[-()----------()-]")
+        print("You're pretty lucky.")
+        next_step()
+    if number==5:
+        print ("[-()----------()-]")
+        print ("[----------------]")
+        print ("[-------()-------]")
+        print ("[----------------]")
+        print ("[-()----------()-]")
+        print("Congrats!")
+        next_step()
+    if number==6:
+        print ("[-()----------()-]")
+        print ("[----------------]")
+        print ("[-()----------()-]")
+        print ("[----------------]")
+        print ("[-()----------()-]")
+        print("Congrats!")
+        next_step()
+
+
+def next_step():
+    print("Here's the next game.")
+    print("You have to get the right answer for this multiple-choice question.")
+    print("You stayed up all night playing video game. And now, you are in Pastor Johnson's class.")
+    print("What should you do?")
+    print("1) sleep")
+    print("2) stand up to get rid of sleepiness")
+    print("3) shout")
+    print("4) ask for permission to go to the toilet")
+    print("Just gimme a number.")
+    choice = raw_input("> ")
+
+    if raw_input == "1":
+        print("OH MAN! No one can sleep in my class!")
+        print("Game Over")
+        exit(0)
+    elif raw_input == "2":
+        print("This is not a Montesory-bang! Sit down!")
+        print("Game Over")
+        exit(0)
+    elif raw_input == "3":
+        print("Well done. You got rid of your exhaustion, and Paul was caught instead of you.")
+        print("All Clear")
+        exit(0)
+    elif raw_input == "4":
+        print("What did you do in the lunch time? No!")
+        print("Game Over")
+        exit(0)
+    else:
+        print("Number, Please.")
+        exit(0)
+
+selfIntroduce()

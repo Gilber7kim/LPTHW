4ea54e393086b90d6630817d0b82152f2f9c45b1
Gilbert Kim <gilbertkim@seoulchristian.org>
2016-02-15 02:55:34 +0900

    Finish checkpoint 3

--- ex43.py
+++ ex43.py
@@ -1,22 +1,45 @@
+# from python system, import exit and randint (random integer)
+from sys import exit
+from random import randint
+
+# a class that defines enter that contains self integer.
 class Scene(object):
-
+# defined enter as not configured syntax.
     def enter(self):
-        pass
+        print "Not configured. Subclass it and implement enter()."
+        exit(1)
 
-
+# a class that consists of as follows:
 class Engine(object):
-
+    # opperates the scene_map by itself.
     def __init__(self, scene_map):
-        pass
-
+        self.scene_map = scene_map
+    #
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
 
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
 
+        current_scene.enter()
+
+#
 class Death(Scene):
 
     def enter(self):
-        pass
+        gg = [
+        "You failed to survive.",
+         "I think you needed some more skill to play this",
+         "Beinging such a coward.",
+         "Well basically you suck."
+    ]
+
+    def enter(self):
+        print Death.gg[randint(0, len(self.gg)-1)]
+        exit(1)
+
 
 
 class CentralCorridor(Scene):
@@ -58,4 +81,3 @@ class Map(object):
 a_map = Map('central_corridor')
 a_game = Engine(a_map)
 a_game.play()
-
